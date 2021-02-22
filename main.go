902f93cdd1e1890cad7820c262154b16918bd74f
Matthew Hirst <hirst.mat@gmail.com>
2021-02-22 20:57:23 +0200

    Cleanup and fix up rand and dollar matching regex.

--- main.go
+++ main.go
@@ -27,11 +27,8 @@ func main() {
 	mainCache = cache.New(30*time.Minute, 10*time.Minute)
 
 	convertRegex := regexp.MustCompile(`(?i)convert ([\d\.]+) (\w+) to (\w+)`)
-	randRegex := regexp.MustCompile(`R([\d\s\.]+)`)
-	dollarRegex := regexp.MustCompile(`\$([\d\s\.]+)`)
-
-	var amount float64
-	var err error
+	randRegex := regexp.MustCompile(`\bR([\d\.]+)\b`)
+	dollarRegex := regexp.MustCompile(`\$([\d\.]+)\b`)
 
 	client.OnConnect(func() {
 		fmt.Println("Connected and listening!")
@@ -39,13 +36,13 @@ func main() {
 
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 		if matched := convertRegex.FindAllStringSubmatch(message.Message, -1); len(matched) > 0 {
-			amount, err = convertResponseHandler(matched[0][2], matched[0][3], matched[0][1])
+			amount, err := convertResponseHandler(matched[0][2], matched[0][3], matched[0][1])
 			errorOrMessage(message.Channel, err, fmt.Sprintf("%s %s IS %.2f %s", matched[0][1], matched[0][2], amount, matched[0][3]))
 		} else if matched := randRegex.FindAllStringSubmatch(message.Message, -1); len(matched) > 0 {
-			amount, err = convertResponseHandler("ZAR", "USD", matched[0][1])
+			amount, err := convertResponseHandler("ZAR", "USD", matched[0][1])
 			errorOrMessage(message.Channel, err, fmt.Sprintf("R%s = $%.2f", matched[0][1], amount))
 		} else if matched := dollarRegex.FindAllStringSubmatch(message.Message, -1); len(matched) > 0 {
-			amount, err = convertResponseHandler("USD", "ZAR", matched[0][1])
+			amount, err := convertResponseHandler("USD", "ZAR", matched[0][1])
 			errorOrMessage(message.Channel, err, fmt.Sprintf("$%s = R%.2f", matched[0][1], amount))
 		}
 	})
@@ -73,12 +70,12 @@ func convertResponseHandler(fromCurrency string, toCurrency string, amountAsStri
 	var err error
 
 	if amount, err = strconv.ParseFloat(amountAsString, 64); err != nil {
-		return 0, fmt.Errorf("Unable to convert %s %s to %s - %s is not a number", amountAsString, fromCurrency, toCurrency, amountAsString)
+		return 0, fmt.Errorf("Unable to convert %q %s to %s - %q is not a number", amountAsString, fromCurrency, toCurrency, amountAsString)
 	}
 
 	convertedAmount, err := fixer.Convert(mainCache, fromCurrency, toCurrency, amount)
 	if err != nil {
-		return 0, fmt.Errorf("Unable to convert %f %s to %s - conversion broke", amount, fromCurrency, toCurrency)
+		return 0, fmt.Errorf("Unable to convert %q %s to %s - conversion broke", amountAsString, fromCurrency, toCurrency)
 	}
 
 	return convertedAmount, nil
